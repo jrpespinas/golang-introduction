4d84ebf965acefd102164cddbe74d7ad27e92663
Bogs Espinas <jrpespinas@gmail.com>
2021-07-31 14:29:35 +0800

    Problem 5

--- problem_set1/11799.go
+++ problem_set1/11799.go
@@ -3,10 +3,17 @@ package main
 import "fmt"
 
 func main() {
-	var c, n, i, max int;
-	fmt.Scan(&c);
-	for i := 1; i <= c; i++ {
-		
-		println("Case %v: %v", i, max);
+	var t, n, s, max int;
+	fmt.Scan(&t);
+	for i := 1; i <= t; i++ {
+		fmt.Scan(&n);
+		max = 0;
+		for j := 0; j < n; j++ {
+			fmt.Scan(&s);
+			if s > max {
+				max = s;
+			}
+		}
+		fmt.Printf("Case %v: %v", i, max);
 	}
 }
